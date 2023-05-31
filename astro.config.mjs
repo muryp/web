c7cfc3ed0258ce7f1453eca6e7732871976aeb61
alifprihantoro <alifprihantoro@gmail.com>
2023-05-31 18:50:02 +0700

    chore: add pkg and initial conf
    pkg add :
    - tailwind
    - eslint
    - markdownit
    - partytown
    
    initial config :
    - tsconfig
    - robot.txt
    - sitemap
    - astro conf from install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://muryp.github.io',
+  integrations: [react(), tailwind(), partytown(), sitemap()]
+});
